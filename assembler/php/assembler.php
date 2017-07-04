e8b55c5d14151fdf9d3fbc6538954d9e086da094
hamza <karlchurchill@gmail.com>
2017-07-04 01:04:11 +0100

    Created instruction lookup and added syntax directive to json structure.

--- assembler/php/assembler.php
+++ assembler/php/assembler.php
@@ -10,7 +10,7 @@ class Assembler {
 
   public function __construct() {
     $this->loadOpcodeDefs();
-    print_r($this);
+    print_r($this->aOpcodeMap);
   }
 
   private function loadOpcodeDefs() {
@@ -21,13 +21,27 @@ class Assembler {
          json_last_error_msg()
        );
      }
+     $this->oSyntax      = $oOpcodeData->syntax;
      $this->aOpcodeProps = (array)$oOpcodeData->opcodes;
      $this->aOpcodeVal   = array_flip(array_keys($this->aOpcodeProps));
+     $sTypeSep           = $oOpcodeData->syntax->typsep;
+     $this->aOpcodeMap = [];
+     foreach ($this->aOpcodeProps as $sOpcode => $oProps) {
+       $sCode = $oProps->mnm;
+       if (isset($oProps->typ)) {
+         foreach ($oProps->typ as $sType) {
+           $this->aOpcodeMap[$sCode . $sTypeSep . $sType] = $sOpcode;
+         }
+       } else {
+         $this->aOpcodeMap[$sCode] = $sOpcode;
+       }
+     }
   }
   
   private
-	$aOpcodeVal,
-	$aOpcodeProps
+    $aOpcodeVal,
+    $aOpcodeProps,
+    $aOpcodeMap
   ;
 }
 
