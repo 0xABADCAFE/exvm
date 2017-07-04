a5362c6a62c09ba7ece344d3a367e17af41d7c96
kchurchill <karl.churchill@gear4music.com>
2017-07-04 19:05:37 +0100

    Transitional changes

--- assembler/php/assembler.php
+++ assembler/php/assembler.php
@@ -5,26 +5,34 @@ namespace ExVM;
 class Assembler {
 
   const
-    OPCODE_DEF = 'opcodes.json'
+    DEFAULT_SPEC_FILE = 'asmspec.json'
   ;
 
-  public function __construct() {
-    $this->loadOpcodeDefs();
-    print_r($this->aOpcodeMap);
+  public function __construct(string $sSpecFile = null) {
+    if (null == $sSpecFile) {
+      $sSpecFile = self::DEFAULT_SPEC_FILE;
+    }
+    $this->loadAssemblerSpec($sSpecFile);
   }
 
-  private function loadOpcodeDefs() {
-     $oOpcodeData = json_decode(file_get_contents(self::OPCODE_DEF));
-     if (!$oOpcodeData) {
+  public function parseLine(string $sLine) {
+
+  }
+
+  private function loadAssemblerSpec(string $sSpecFile) {
+     $oAssemblerSpec = json_decode(file_get_contents(self::OPCODE_DEF));
+     if (!$oAssemblerSpec) {
        throw new \Exception(
-         'Unable to load "' . self::OPCODE_DEF . '", ' .
+         'Unable to load "' . self::SPEC_FILE . '", ' .
          json_last_error_msg()
        );
      }
-     $this->oSyntax      = $oOpcodeData->syntax;
-     $this->aOpcodeProps = (array)$oOpcodeData->opcodes;
+
+     $this->aReg = (array)$oAssemblerSpec->reg;
+
+     $this->aOpcodeProps = (array)$oAssemblerSpec->opcodes;
      $this->aOpcodeVal   = array_flip(array_keys($this->aOpcodeProps));
-     $sTypeSep           = $oOpcodeData->syntax->typsep;
+     $sTypeSep           = $oAssemblerSpec->param->type;
      $this->aOpcodeMap = [];
      foreach ($this->aOpcodeProps as $sOpcode => $oProps) {
        $sCode = $oProps->mnemonic;
@@ -40,9 +48,11 @@ class Assembler {
   
   private
     $aOpcodeVal,
-    $aOpcodeProps,
-    $aOpcodeMap
+    $aOpcodeMap,
+    $aOpcodeProps
   ;
 }
 
+
 $oAssembler = new Assembler();
+print_r($oAssembler);
