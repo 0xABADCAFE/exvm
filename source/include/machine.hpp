cde65c0421c7e012067baab5ae7e0c0f5a99e76b
kchurchill <karl.churchill@gear4music.com>
2017-07-11 11:26:09 +0100

    Fixed bug in machine include that broke WarpOS build.

--- source/include/machine.hpp
+++ source/include/machine.hpp
@@ -13,8 +13,8 @@
 //****************************************************************************//
 
 #ifndef _VM_HOST_MACHINE_HPP_
-#define _VM_HOST_MACHINE_HPP_
-#include <cstdlib>
+  #define _VM_HOST_MACHINE_HPP_
+  #include <cstdlib>
 
 // this is just a gap filler for framework xbase definitions
 
@@ -33,102 +33,101 @@ any pointer %p
 
 */
 
-#define XA_BIGENDIAN 0
-#define XA_LITTLEENDIAN 1
-#define XA_PTRSIZE32 32
-#define XA_PTRSIZE64 64
+  #define XA_BIGENDIAN 0
+  #define XA_LITTLEENDIAN 1
+  #define XA_PTRSIZE32 32
+  #define XA_PTRSIZE64 64
 
-// change this for other systems
-#define _VM_HOST_AMIGAOS3_68K     1
-#define _VM_HOST_AMIGAOS3_WARPUP  2
-#define _VM_HOST_LINUX_I386       3
-#define _VM_HOST_WIN32_I386       4
-#define _VM_HOST_LINUX_X64        5
-#define _VM_HOST_AMIGAOS4_PPC     6
-#define _VM_HOST_MORPHOS_PPC      7
+  // change this for other systems
+  #define _VM_HOST_AMIGAOS3_68K     1
+  #define _VM_HOST_AMIGAOS3_WARPUP  2
+  #define _VM_HOST_LINUX_I386       3
+  #define _VM_HOST_WIN32_I386       4
+  #define _VM_HOST_LINUX_X64        5
+  #define _VM_HOST_AMIGAOS4_PPC     6
+  #define _VM_HOST_MORPHOS_PPC      7
 
 
-#define _VM_INTERPRETER_FUNC_TABLE     1
-#define _VM_INTERPRETER_SWITCH_CASE    2
-#define _VM_INTERPRETER_CUSTOM_ASM     3
+  #define _VM_INTERPRETER_FUNC_TABLE     1
+  #define _VM_INTERPRETER_SWITCH_CASE    2
+  #define _VM_INTERPRETER_CUSTOM_ASM     3
 
-#ifdef __LP64__
-  #define X_PTRSIZE XA_PTRSIZE64
-typedef signed char sint8;
-typedef signed short int sint16;
-typedef signed int sint32;
-typedef signed long int sint64;
-typedef unsigned char uint8;
-typedef unsigned short int uint16;
-typedef unsigned int uint32;
-typedef unsigned long int uint64;
-typedef float float32;
-typedef double float64;
-#else
-  #define X_PTRSIZE XA_PTRSIZE32
-typedef signed char sint8;
-typedef signed short int sint16;
-typedef signed long int sint32;
-typedef signed long long int sint64;
-typedef unsigned char uint8;
-typedef unsigned short int uint16;
-typedef unsigned long int uint32;
-typedef unsigned long long int uint64;
-typedef float float32;
-typedef double float64;
-#endif
-
-#define rsint8 register sint8
-#define rsint16 register sint16
-#define rsint32 register sint32
-#define rsint64 register sint64
-#define ruint8 register uint8
-#define ruint16 register uint16
-#define ruint32 register uint32
-#define ruint64 register uint64
-#define rfloat32 register float32
-#define rfloat64 register float64
-
-// Target sanity checks
-
-#if (_VM_HOST_OS == _VM_HOST_AMIGAOS3_68K)
-  #ifdef __PPC__
-    #warning PowerPC directive set for AmigaOS 680x0 target, switching to WarpOS
-    #undef _VM_HOST_OS
-    #define _VM_HOST_OS _VM_HOST_AMIGAOS3_WARPUP
-  #endif
-#endif
-
-#if (_VM_HOST_OS == _VM_HOST_AMIGAOS3_WARPUP) || (_VM_HOST_OS == _VM_HOST_AMIGAOS3_68K)
   #ifdef __LP64__
-    #error 64-bit directive set for AmigaOS 3.x target, no known supported 64-bit architecture
+    #define X_PTRSIZE XA_PTRSIZE64
+    typedef signed char sint8;
+    typedef signed short int sint16;
+    typedef signed int sint32;
+    typedef signed long int sint64;
+    typedef unsigned char uint8;
+    typedef unsigned short int uint16;
+    typedef unsigned int uint32;
+    typedef unsigned long int uint64;
+    typedef float float32;
+    typedef double float64;
+  #else
+    #define X_PTRSIZE XA_PTRSIZE32
+    typedef signed char sint8;
+    typedef signed short int sint16;
+    typedef signed long int sint32;
+    typedef signed long long int sint64;
+    typedef unsigned char uint8;
+    typedef unsigned short int uint16;
+    typedef unsigned long int uint32;
+    typedef unsigned long long int uint64;
+    typedef float float32;
+    typedef double float64;
   #endif
-#endif
 
-#if (_VM_HOST_OS == _VM_HOST_LINUX_I386)
-  #ifdef __LP64__
-    #warning 64-bit directive set for Linux i386 target, switching to x64
-    #undef _VM_HOST_OS
-    #define _VM_HOST_OS _VM_HOST_LINUX_X64
+  #define rsint8 register sint8
+  #define rsint16 register sint16
+  #define rsint32 register sint32
+  #define rsint64 register sint64
+  #define ruint8 register uint8
+  #define ruint16 register uint16
+  #define ruint32 register uint32
+  #define ruint64 register uint64
+  #define rfloat32 register float32
+  #define rfloat64 register float64
+
+  // Target sanity checks
+  #if (_VM_HOST_OS == _VM_HOST_AMIGAOS3_68K)
+    #ifdef __PPC__
+      #warning PowerPC directive set for AmigaOS 680x0 target, switching to WarpOS
+      #undef _VM_HOST_OS
+      #define _VM_HOST_OS _VM_HOST_AMIGAOS3_WARPUP
+    #endif
   #endif
+
+  #if (_VM_HOST_OS == _VM_HOST_AMIGAOS3_WARPUP) || (_VM_HOST_OS == _VM_HOST_AMIGAOS3_68K)
+    #ifdef __LP64__
+      #error 64-bit directive set for AmigaOS 3.x target, no known supported 64-bit architecture
+    #endif
+  #endif
+
+  #if (_VM_HOST_OS == _VM_HOST_LINUX_I386)
+    #ifdef __LP64__
+      #warning 64-bit directive set for Linux i386 target, switching to x64
+      #undef _VM_HOST_OS
+      #define _VM_HOST_OS _VM_HOST_LINUX_X64
+    #endif
+  #endif
+
+  // target specific class implementations
+  #if (_VM_HOST_OS == _VM_HOST_LINUX_I386) || (_VM_HOST_OS == _VM_HOST_LINUX_X64)
+    #define X_ENDIAN XA_LITTLEENDIAN
+    #include "platforms/machine_linux_generic.hpp"
+  #elif _VM_HOST_OS == _VM_HOST_WIN32_I386
+    #warning This target is almost certainly not working
+    #include "platforms/machine_win32.hpp"
+  #elif (_VM_HOST_OS == _VM_HOST_AMIGAOS3_68K)
+    #include "platforms/machine_amiga_68k.hpp"
+  #elif (_VM_HOST_OS == _VM_HOST_AMIGAOS3_WARPUP)
+    #include "platforms/machine_amiga_wos.hpp"
+  #elif (_VM_HOST_OS == _VM_HOST_AMIGAOS4_PPC)
+    #include "platforms/machine_amiga_os4.hpp"
+  #else
+    #error Unrecognised Machine
+  #endif
+
 #endif
 
-// target specific class implementations
-
-#if (_VM_HOST_OS == _VM_HOST_LINUX_I386) || (_VM_HOST_OS == _VM_HOST_LINUX_X64)
-  #define X_ENDIAN XA_LITTLEENDIAN
-  #include "platforms/machine_linux_generic.hpp"
-#elif _VM_HOST_OS == _VM_HOST_WIN32_I386
-  #warning This target is almost certainly not working
-  #include "platforms/machine_win32.hpp"
-#elif (_VM_HOST_OS == _VM_HOST_AMIGAOS3_68K)
-  #include "platforms/machine_amiga_68k.hpp"
-#elif (_VM_HOST_OS == _VM_HOST_AMIGAOS3_WARPUP)
-  #include "platforms/machine_amiga_68k.hpp"
-#elif (_VM_HOST_OS == _VM_HOST_AMIGAOS4_PPC)
-  #include "platforms/machine_amiga_os4.hpp"
-#else
-  #error Unrecognised Machine
-#endif
-
-#endif
