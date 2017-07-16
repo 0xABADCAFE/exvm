1e69bb9f46d7bc9d4d4925a93cfed7c88e3ff0b8
Hamza <karlchurchill@gmail.com>
2017-07-16 21:47:35 +0100

    Initial files for linker. Nothing to see here.

---
+++ source/include/vm_linker.hpp
@@ -0,0 +1,47 @@
+//****************************************************************************//
+//**                                                                        **//
+//** File:         vm_linker.hpp                                            **//
+//** Description:  Linker definitions                                       **//
+//** Comment(s):   Internal developer version only                          **//
+//** Library:                                                               **//
+//** Created:      2017-07-12                                               **//
+//** Author(s):    Karl Churchill                                           **//
+//** Note(s):                                                               **//
+//** Copyright:    (C)1996 - , eXtropia Studios                             **//
+//**               All Rights Reserved.                                     **//
+//**                                                                        **//
+//****************************************************************************//
+
+#ifndef _VM_LINKER_HPP_
+  #define _VM_LINKER_HPP_
+  #include "vm_symbol.hpp"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// VMLinker
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class VMLinker {
+
+  public:
+    enum {
+      MAX_SYMBOLS_NATIVE = 65536,
+      MAX_SYMBOLS_CODE   = 65536,
+      MAX_SYMBOLS_DATA   = 65536,
+    };
+
+  int registerNative(const char* symbol, VMCore::Native func);
+  int registerCode(const char* symbol, const uint16* func);
+  int registerData(const char* symbol, const void* data);
+
+  VMLinker();
+  ~VMLinker();
+
+  private:
+    VMSymbolTable* native;
+    VMSymbolTable* code;
+    VMSynbolTable* data;
+};
+
+#endif
