4459414d9bcf903be17c0fa04ae0d7b8ac479130
Hamza <karlchurchill@gmail.com>
2017-08-20 15:47:36 +0100

    Disabling unimplemented saturating vector opcodes for now.

--- source/include/op_vec_sadd_saturated_impl.hpp
+++ source/include/op_vec_sadd_saturated_impl.hpp
@@ -44,8 +44,8 @@ _END_OP
 _DEFINE_OP(VSADDS_U32) {
   // Super naive reference implementation
   uint32  val = vm->gpr[(vArgs & 0xF000) >> 12].u32();
-  uint16* src = vm->gpr[(vArgs & 0x0F00) >>  8].pU32();
-  uint16* dst = vm->gpr[(vArgs & 0x00F0) >>  4].pU32();
+  uint32* src = vm->gpr[(vArgs & 0x0F00) >>  8].pU32();
+  uint32* dst = vm->gpr[(vArgs & 0x00F0) >>  4].pU32();
   uint32  i   = vm->gpr[(vArgs & 0x000F)].u32();
   while (i--) {
     // TODO - validate correctness
@@ -72,53 +72,64 @@ _END_OP
 
 
 _DEFINE_OP(VSADDS_S8) {
-  // Super naive reference implementation
-  sint8   val = vm->gpr[(vArgs & 0xF000) >> 12].s8();
-  sint8*  src = vm->gpr[(vArgs & 0x0F00) >>  8].pS8();
-  sint8*  dst = vm->gpr[(vArgs & 0x00F0) >>  4].pS8();
-  uint32  i   = vm->gpr[(vArgs & 0x000F)].u32();
-  while (i--) {
-    // TODO - validate correctness
+  vm->status = VMDefs::BREAKPOINT;
+  return;
 
-  }
+//   // Super naive reference implementation
+//   sint8   val = vm->gpr[(vArgs & 0xF000) >> 12].s8();
+//   sint8*  src = vm->gpr[(vArgs & 0x0F00) >>  8].pS8();
+//   sint8*  dst = vm->gpr[(vArgs & 0x00F0) >>  4].pS8();
+//   uint32  i   = vm->gpr[(vArgs & 0x000F)].u32();
+//   while (i--) {
+//     // TODO - validate correctness
+//
+//  }
 }
 _END_OP
 
 _DEFINE_OP(VSADDS_S16) {
-  // Super naive reference implementation
-  sint16  val = vm->gpr[(vArgs & 0xF000) >> 12].s16();
-  sint16* src = vm->gpr[(vArgs & 0x0F00) >>  8].pS16();
-  sint16* dst = vm->gpr[(vArgs & 0x00F0) >>  4].pS16();
-  uint32  i   = vm->gpr[(vArgs & 0x000F)].u32();
-  while (i--) {
-    // TODO - validate correctness
+  vm->status = VMDefs::BREAKPOINT;
+  return;
 
-  }
+//   // Super naive reference implementation
+//   sint16  val = vm->gpr[(vArgs & 0xF000) >> 12].s16();
+//   sint16* src = vm->gpr[(vArgs & 0x0F00) >>  8].pS16();
+//   sint16* dst = vm->gpr[(vArgs & 0x00F0) >>  4].pS16();
+//   uint32  i   = vm->gpr[(vArgs & 0x000F)].u32();
+//   while (i--) {
+//     // TODO - validate correctness
+//
+//   }
 }
 _END_OP
 
 _DEFINE_OP(VSADDS_S32) {
-  // Super naive reference implementation
-  sint32  val = vm->gpr[(vArgs & 0xF000) >> 12].s32();
-  sint16* src = vm->gpr[(vArgs & 0x0F00) >>  8].pS32();
-  sint16* dst = vm->gpr[(vArgs & 0x00F0) >>  4].pS32();
-  uint32  i   = vm->gpr[(vArgs & 0x000F)].u32();
-  while (i--) {
-    // TODO - validate correctness
+  vm->status = VMDefs::BREAKPOINT;
+  return;
 
-  }
+//   // Super naive reference implementation
+//   sint32  val = vm->gpr[(vArgs & 0xF000) >> 12].s32();
+//   sint32* src = vm->gpr[(vArgs & 0x0F00) >>  8].pS32();
+//   sint32* dst = vm->gpr[(vArgs & 0x00F0) >>  4].pS32();
+//   uint32  i   = vm->gpr[(vArgs & 0x000F)].u32();
+//   while (i--) {
+//     // TODO - validate correctness
+//
+//   }
 }
 _END_OP
 
 _DEFINE_OP(VSADDS_S64) {
-  // Super naive reference implementation
-  sint64  val = vm->gpr[(vArgs & 0xF000) >> 12].s64();
-  sint64* src = vm->gpr[(vArgs & 0x0F00) >>  8].pS64();
-  sint64* dst = vm->gpr[(vArgs & 0x00F0) >>  4].pS64();
-  uint32  i   = vm->gpr[(vArgs & 0x000F)].u32();
-  while (i--) {
-    // TODO - validate correctness
-
-  }
+  vm->status = VMDefs::BREAKPOINT;
+  return;
+//   // Super naive reference implementation
+//   sint64  val = vm->gpr[(vArgs & 0xF000) >> 12].s64();
+//   sint64* src = vm->gpr[(vArgs & 0x0F00) >>  8].pS64();
+//   sint64* dst = vm->gpr[(vArgs & 0x00F0) >>  4].pS64();
+//   uint32  i   = vm->gpr[(vArgs & 0x000F)].u32();
+//   while (i--) {
+//     // TODO - validate correctness
+//
+//   }
 }
 _END_OP
