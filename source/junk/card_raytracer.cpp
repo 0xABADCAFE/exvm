f4f1a5b25a62fe8deda100de1f9fefae9d7c4e0f
kchurchill <karl.churchill@gear4music.com>
2017-09-20 18:12:57 +0100

    Removing operator overloads from tracer. Ironically I think this is less readable.

--- source/junk/card_raytracer.cpp
+++ source/junk/card_raytracer.cpp
@@ -21,18 +21,8 @@ struct vec3 {
     return vec3(x * r, y * r, z * r);
   }
 
-  // dot product
-  float32 operator%(const vec3& r) const {
-    return x * r.x + y * r.y + z * r.z;
-  }
-
   // default constructor
   vec3() { }
-
-  // Cross product
-  vec3 operator^(const vec3& r) const {
-    return vec3(y * r.z - z * r.y, z * r.x - x * r.z, x * r.y - y * r.x);
-  }
 
   // constructor
   vec3(float32 a, float32 b, float32 c) {
@@ -41,11 +31,9 @@ struct vec3 {
     z = c;
   }
 
-  // Normalise
-  vec3 operator!() const {
-    return *this * (1.0 / sqrt((x * x) + (y * y) + (z * z)));
-  }
 };
+
+
 
 // Data
 int32 data[] = {
@@ -60,10 +48,38 @@ int32 data[] = {
   16
 };
 
+///////////////////////////////////////////////////////////////////////////////
+
+static vec3 scale(const vec3& v, float32 s) {
+  return vec3(v.x * s, v.y * s, v.z * s);
+}
+
+static vec3 normalize(const vec3& v) {
+  return scale(v, (1.0 / sqrt((v.x * v.x) + (v.y * v.y) + (v.z * v.z))));
+}
+
+static float32 dot(const vec3& v1, const vec3& v2) {
+  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+static vec3 cross(const vec3& v1, const vec3& v2) {
+  return vec3(
+    v1.y * v2.z - v1.z * v2.y,
+    v1.z * v2.x - v1.x * v2.z,
+    v1.x * v2.y - v1.y * v2.x
+  );
+}
+
+
+
+static const float32 invRM = 1.0 / RAND_MAX;
+
 // Random
 static inline float32 frand() {
-  return (float32) rand() / RAND_MAX;
+  return invRM * rand();
 }
+
+///////////////////////////////////////////////////////////////////////////////
 
 // Trace
 int32 trace(const vec3& o, const vec3& d, float32& t, vec3& n) {
@@ -80,15 +96,15 @@ int32 trace(const vec3& o, const vec3& d, float32& t, vec3& n) {
       if (data[j] & 1 << k) {
         vec3 p = o + vec3(-k, 0.0, -j - 4.0);
         float32
-          b = p % d,
-          c = p % p - 1.0,
+          b = dot(p, d),
+          c = dot(p, p) - 1.0,
           q = b * b - c
         ;
         if (q > 0) {
           float32 s = -b - sqrt(q);
           if (s < t && s > 0.01) {
             t = s,
-            n = !(p + d * t),
+            n = normalize(p + d * t),
             m = 2;
           }
         }
@@ -97,6 +113,8 @@ int32 trace(const vec3& o, const vec3& d, float32& t, vec3& n) {
   }
   return m;
 }
+
+///////////////////////////////////////////////////////////////////////////////
 
 // Sampling
 vec3 sample(const vec3& o, const vec3& d) {
@@ -108,38 +126,41 @@ vec3 sample(const vec3& o, const vec3& d) {
 
   // No intersection, sky
   if (!m) {
-    return vec3(0.7, 0.6, 1.0) * pow(1.0 - d.z, 4.0);
+    return scale(vec3(0.7, 0.6, 1.0), pow(1.0 - d.z, 4.0));
   }
 
   vec3
-    h = o + d * t,
-    l = !(vec3(9.0 + frand(), 9.0 + frand(), 16.0) + h * -1.0),
-    r = d + n * (n % d * -2)
+    h = o + scale(d, t),
+    l = normalize(vec3(9.0 + frand(), 9.0 + frand(), 16.0) + h * -1.0),
+    r = d + scale(n, (dot(n, d) * -2.0))
   ;
 
-  float32 b = l % n;
+  float32 b = dot(l, n);
   if (b < 0 || trace(h, l, t, n)) {
     b = 0;
   }
 
-  float32 p = pow(l % r * (b > 0.0), 99.0);
+  float32 p = pow(dot(l, r) * (b > 0.0), 99.0);
+
+  // Floor plane
   if (m & 1) {
-    h = h * 0.2;
+    h = scale(h, 0.2);
     return ( (int32) (ceil(h.x) + ceil(h.y)) & 1 ? vec3(3.0, 1.0, 1.0) : vec3(3.0, 3.0, 3.0)) * (b * 0.2 + 0.1);
   }
-  return vec3(p, p, p) + sample(h, r) * 0.5;
+  return vec3(p, p, p) + scale(sample(h, r), 0.5);
 }
 
+///////////////////////////////////////////////////////////////////////////////
 
 // Main
 int32 main() {
   printf("P6 512 512 255 ");
   // camera direction
   vec3
-    g = !vec3(-6.0, -16.0, 0.0),
-    a = !(vec3(0.0, 0.0, 1.0) ^ g) * 0.002,
-    b = !(g ^ a) * 0.002,
-    c = (a + b) * -256 + g
+    g = normalize( vec3(-6.0, -16.0, 0.0) ),
+    a = scale(normalize( cross( vec3(0.0, 0.0, 1.0), g) ), 0.002),
+    b = scale(normalize( cross(g, a) ), 0.002),
+    c = scale( (a + b), -256.0) + g
   ;
   for (int32 y = 512; y--;) {
     for (int32 x = 512; x--;) {
@@ -147,10 +168,12 @@ int32 main() {
 
       // Cast 64 rays per pixel
       for (int32 r = 64; r--;) {
-        vec3 t = a * (frand() - 0.5) * 99.0 + b * (frand() - 0.5) * 99.0;
-        pixel = sample(
+        vec3 t = scale(a, (frand() - 0.5) * 99.0) + scale(b, (frand() - 0.5) * 99.0);
+        pixel  = sample(
           vec3(17.0, 16.0, 8.0) + t,
-          !(t * -1.0 + (a * (frand() + x) + b * (y + frand()) + c) * 16.0)
+          normalize(
+            scale(t, -1.0) + scale(( a * (frand() + x) + b * (y + frand()) + c), 16.0)
+          )
         ) * 3.5 + pixel;
       }
       printf("%c%c%c", (int32)pixel.x, (int32)pixel.y, (int32)pixel.z);
