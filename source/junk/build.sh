8ac0cabdc9208ae107b591e6339600c32da53909
kchurchill <karl.churchill@gear4music.com>
2017-09-22 17:13:14 +0100

    Fixed junk build file

--- source/junk/build.sh
+++ source/junk/build.sh
@@ -3,5 +3,5 @@ g++ -O3 -funroll-loops -fwhole-program card_original.cpp -o card_original
 g++ -O3 -funroll-loops -fwhole-program card_original.cpp -S
 g++ -O3 -funroll-loops -fwhole-program card_raytracer.cpp -o card_raytracer
 g++ -O3 -funroll-loops -fwhole-program card_raytracer.cpp -S
-g++ -O3 -funroll-loops -fwhole-program card_raytracer_vec.cpp -o card_raytracer_noref_vec
+g++ -O3 -funroll-loops -fwhole-program card_raytracer_vec.cpp -o card_raytracer_vec
 g++ -O3 -funroll-loops -fwhole-program card_raytracer_vec.cpp -S
