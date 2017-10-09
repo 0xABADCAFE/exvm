cfeeee2feb973ac2ae9194209a538fce01aad997
kchurchill <karl.churchill@gear4music.com>
2017-10-09 18:10:53 +0100

    Removed unnecessary java.nio.ByteBuffer usage in relative performance test of Java target. Updated makefile for relative performance tests to use -march=native, restoring some sanity.

--- source/junk/build.sh
+++ source/junk/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-g++ -O3 -funroll-loops -fwhole-program card_original.cpp -o bin/card_original
+g++ -O3 -march=native -fomit-frame-pointer -funroll-loops -fwhole-program card_original.cpp -o bin/card_original
 #g++ -O3 -funroll-loops -fwhole-program card_original.cpp -S
-g++ -O3 -funroll-loops -fwhole-program card_raytracer.cpp -o bin/card_raytracer
+g++ -O3 -march=native -fomit-frame-pointer -funroll-loops -fwhole-program card_raytracer.cpp -o bin/card_raytracer
 #g++ -O3 -funroll-loops -fwhole-program card_raytracer.cpp -S
-g++ -O3 -funroll-loops -fwhole-program card_raytracer_vec.cpp -o bin/card_raytracer_vec
+g++ -O3 -march=native -fomit-frame-pointer -funroll-loops -fwhole-program card_raytracer_vec.cpp -o bin/card_raytracer_vec
 #g++ -O3 -funroll-loops -fwhole-program card_raytracer_vec.cpp -S
-gcc -O3 -funroll-loops -fwhole-program mandelbrot.c -o bin/mandelbrot
+gcc -O3 -march=native -fomit-frame-pointer -funroll-loops -fwhole-program mandelbrot.c -o bin/mandelbrot
 #gcc -O3 -funroll-loops -fwhole-program mandelbrot.c -S
 javac ./Mandelbrot.java -d bin/
