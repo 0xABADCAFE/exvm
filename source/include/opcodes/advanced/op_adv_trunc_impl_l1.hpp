245b33c0c7314d380a228b778d2ff1f1eecea66a
kchurchill <karl.churchill@gear4music.com>
2017-10-12 19:13:59 +0100

    Splitting last set of advanced opcode includes.

--- source/include/opcodes/advanced/op_adv_trunc_impl_l1.hpp
+++ source/include/opcodes/advanced/op_adv_trunc_impl_l1.hpp
@@ -20,20 +20,8 @@ _DEFINE_OP(CEIL_F32) {
 }
 _END_OP
 
-
-_DEFINE_OP(CEIL_F64) {
-  vm->gpr[(vArgs & 0x000F)].f64() = std::ceil(vm->gpr[(vArgs & 0x00F0) >> 4].f64());
-}
-_END_OP
-
-
 _DEFINE_OP(FLOOR_F32) {
   vm->gpr[(vArgs & 0x000F)].f32() = (float32)std::floor((float64)vm->gpr[(vArgs & 0x00F0) >> 4].f32());
 }
 _END_OP
 
-
-_DEFINE_OP(FLOOR_F64) {
-  vm->gpr[(vArgs & 0x000F)].f64() = std::floor(vm->gpr[(vArgs & 0x00F0) >> 4].f64());
-}
-_END_OP
